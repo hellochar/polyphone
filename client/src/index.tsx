5d1e8b98a05920896e66ab5b6bbdfcbaf71d0a62
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-09-11 15:55:00 -0700

    basic firebase integration
    create firebase app + database; test read/write integrating from React

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,84 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import io from "socket.io-client";
+import * as firebase from "firebase";
 
-const App = () => (
+class App extends React.Component<{db: firebase.database.Database}, {}> {
+    render() {
+        return (
+            <div>
+                <h1>Polyphone.io</h1>
+                <FooListener db={db}/>
+            </div>
+        );
+    }
+};
+
+class FooListener extends React.PureComponent<{db: firebase.database.Database}, { val?: any }> {
+    state = { val: undefined };
+
+    private ref: firebase.database.Reference;
+    constructor(props: any, context?: any) {
+        super(props, context);
+        this.ref = this.props.db.ref("foo");
+        this.ref.on("value", (snapshot) => {
+            console.log(snapshot);
+            if (snapshot != null) {
+                this.setState({
+                    val: snapshot.val(),
+                });
+            } else {
+                this.setState({
+                    val: undefined,
+                })
+            }
+        });
+    }
+
+    public render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>+</button>
+                <FooRenderer val={this.state.val} />
+            </div>
+        );
+    }
+
+    private handleClick = () => {
+        this.ref.set(this.state.val! + 1);
+    }
+}
+
+const FooRenderer: React.StatelessComponent<{val: any}> = ({ val }) => (
     <div>
-        <h1>Polyphone.io</h1>
+        Foo is:
+        <pre>{JSON.stringify(val)}</pre>
     </div>
 );
 
+const server = io();
+server.send("hello");
+
+const config = {
+    apiKey: "AIzaSyBT3hTYRj0u-ApZE1_Z1fyXf2ZiV9mgXr0",
+    authDomain: "polyphone-io.firebaseapp.com",
+    databaseURL: "https://polyphone-io.firebaseio.com",
+    projectId: "polyphone-io",
+    storageBucket: "polyphone-io.appspot.com",
+    messagingSenderId: "255218178256"
+};
+firebase.initializeApp(config);
+
+const db = firebase.database();
+// console.log(db);
+
+// const fooRef = db.ref("foo");
+// fooRef.on("value", (snapshot) => {
+//     if (snapshot != null) {
+//         snapshot.val;
+//     }
+// });
+
 const root = document.getElementById("root");
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(<App db={db} />, root);
