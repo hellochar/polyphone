31a00b3cf140200bcc5483cd6933ef11b8435f81
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-09-11 15:00:33 -0700

    client renders w/ react, ts, webpack
    server sets up websockets client

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App = () => (
+    <div>
+        <h1>Polyphone.io</h1>
+    </div>
+);
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<App />, root);
---
+++ client/webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    disableHostCheck: true,
+    host: "0.0.0.0",
+    hot: true,
+    historyApiFallback: true,
+    useLocalIp: true,
+  },
+  entry: {
+    main: path.join(__dirname, "/src/index.tsx"),
+  },
+  output: {
+    publicPath: "/",
+    path: path.resolve(__dirname, "public"),
+    filename: "[name].js",
+    chunkFilename: '[name].bundle.js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          { loader: 'css-loader', options: { importLoaders: 2 } },
+          "postcss-loader",
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(vert|frag|glsl)$/,
+        use: "webpack-glsl-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      'three-examples': path.resolve(__dirname, 'node_modules/three/examples/js/')
+    }
+  },
+  plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            inlineSource: ".css$",
+            template: path.resolve(__dirname, './index.template.html'),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackInlineSourcePlugin(),
+  ]
+};
